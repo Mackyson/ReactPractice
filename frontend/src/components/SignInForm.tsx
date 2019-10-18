34d7441079a4a9086d654f8222e9616b541ef6cc
Mackyson <mackyson72@gmail.com>
2019-10-19 01:16:19 +0900

    コンポーネントの読み込みをテスト

---
+++ frontend/src/components/SignInForm.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default class SignInForm extends React.Component {
+	render() {
+		return <div>ログイン画面</div>
+	}
+}
---
+++ frontend/src/index.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import SignInForm from "./components/SignInForm.tsx"
+
+ReactDOM.render(<SignInForm />, document.querySelector("#root"))
