c3ccbdb674895bc6af31ac99fe119b75df2cffb6
Mackyson <mackyson72@gmail.com>
2019-10-18 21:27:51 +0900

    commit時にステージングされたJSファイルをチェック/整形してくれる(はず)

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,17 +1,19 @@
 module.exports = {
-  entry: './src/index.jsx', // エントリポイントのjsxファイル
-  output: {
-    path: `${__dirname}`+'/public',
-    filename: 'bundle.js' // 出力するファイル
-  },
-  module: {
-    rules: [{
-      test: /\.js(x?)$/, //jsファイル，またはjsxファイル
-      exclude: /node_modules/, // node_modulesフォルダ配下でなければ
-      loader: 'babel-loader', // babel-loaderを使って変換する
-      query: {
-        plugins: ["transform-react-jsx"] // babelのtransform-react-jsxプラグインを使ってjsxを変換
-      }
-    }]
-  }
+	entry: "./src/index.jsx", // エントリポイントのjsxファイル
+	output: {
+		path: `${__dirname}` + "/public",
+		filename: "bundle.js", // 出力するファイル
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(j|t)s(x?)$/, //jsファイル，またはjsxファイル
+				exclude: /node_modules/, // node_modulesフォルダ配下でなければ
+				loader: "babel-loader", // babel-loaderを使って変換する
+				query: {
+					plugins: ["transform-react-jsx"], // babelのtransform-react-jsxプラグインを使ってjsxを変換
+				},
+			},
+		],
+	},
 }
