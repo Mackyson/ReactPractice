6bdbe169f9fc2f21e8ead820d7cb5f8b70c068f0
Mackyson <mackyson72@gmail.com>
2019-10-20 01:06:42 +0900

    カウントアップボタン作ってみた

--- frontend/src/components/SignInForm.tsx
+++ frontend/src/components/SignInForm.tsx
@@ -1,7 +1,22 @@
 import React from "react"
+import RaisedButton from "material-ui/RaisedButton"
 
 export default class SignInForm extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			cnt: 0,
+		}
+	}
+	handleClick = () => {
+		this.setState({ cnt: this.state.cnt + 1 })
+	}
 	render() {
-		return <div>ログイン画面</div>
+		return (
+			<div>
+				ログイン画面
+				<RaisedButton label={this.state.cnt} onClick={this.handleClick} />
+			</div>
+		)
 	}
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,11 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider"
 import SignInForm from "./components/SignInForm.tsx"
 
-ReactDOM.render(<SignInForm />, document.querySelector("#root"))
+ReactDOM.render(
+	<MuiThemeProvider>
+		<SignInForm />
+	</MuiThemeProvider>,
+	document.querySelector("#root")
+)
