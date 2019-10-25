0ad6e209160de0e9debfa96def2894ec1b40750e
Mackyson <mackyson72@gmail.com>
2019-10-25 18:23:32 +0900

    日時選択を追加

--- frontend/src/components/TaskAddingForm.tsx
+++ frontend/src/components/TaskAddingForm.tsx
@@ -4,12 +4,18 @@ import { TextField, Button } from "@material-ui/core/"
 export default class TaskAddingForm extends React.Component {
 	constructor(props) {
 		super(props)
+		let nowDate = new Date().toISOString().split("T")[0] //yyyy-mm-dd
 		this.state = {
+			date: nowDate,
 			task: "",
+			uid: -1,
 		}
 	}
-	inputTask = e => {
+	inputContent = e => {
 		this.setState({ task: e.target.value })
+	}
+	inputDate = e => {
+		this.setState({ date: e.target.value })
 	}
 	submit = () => {
 		console.log(this.state.task, " をしようね！")
@@ -19,7 +25,16 @@ export default class TaskAddingForm extends React.Component {
 	render() {
 		return (
 			<div>
-				<TextField value={this.state.task} onChange={this.inputTask} placeholder="e.g. 進捗を出す" margin="none" />
+				<TextField value={this.state.task} onChange={this.inputContent} placeholder="e.g. 進捗を出す" margin="none" />
+				<TextField
+					id="date"
+					type="date"
+					onChange={this.inputDate}
+					value={this.state.date}
+					InputLabelProps={{
+						shrink: true,
+					}}
+				/>
 				<Button variant="contained" onClick={this.submit.bind(this)}>
 					タスク追加
 				</Button>
